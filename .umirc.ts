af3942e8924ecc6d8bafe4ae5174c1b4026cf495
60995360605748651 <1615258386@qq.com>
2023-12-29 15:28:51 +0800

    hhh

---
+++ .umirc.ts
@@ -0,0 +1,33 @@
+import { defineConfig } from '@umijs/max';
+
+export default defineConfig({
+  antd: {},
+  access: {},
+  model: {},
+  initialState: {},
+  request: {},
+  layout: false,
+  routes: [
+    {
+      path: '/',
+      redirect: '/hoc',
+    },
+    {
+      name: '首页',
+      path: '/hoc',
+      component: './hoc',
+    },
+    // {
+    //   name: '权限演示',
+    //   path: '/access',
+    //   component: './Access',
+    // },
+    // {
+    //   name: ' CRUD 示例',
+    //   path: '/table',
+    //   component: './Table',
+    // },
+  ],
+  npmClient: 'pnpm',
+});
+
---
+++ src/pages/hoc/index.jsx
@@ -0,0 +1,10 @@
+import { Form, Input } from 'antd-mobile'
+import styles from './demo2.less';
+
+export default () => {
+    return (
+        <div className={styles['demo']}>
+            啦啦啦
+        </div>
+    )
+}
