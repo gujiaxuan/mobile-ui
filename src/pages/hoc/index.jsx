0e824bbec2fbe592e8d06e1661f72777528c4f06
gujiaxuan <1615258386@qq.com>
2024-01-02 15:17:52 +0800

    test(custom): test lint

--- src/pages/hoc/index.jsx
+++ src/pages/hoc/index.jsx
@@ -1,11 +1,9 @@
-import { useEffect, useState, useCallback } from 'react';
+import { useEffect, useState } from 'react';
 import styles from './demo2.less';
 
 export default () => {
   const [hh, setHh] = useState({});
 
-
-  
   useEffect(() => {
     if (hh) {
       setHh('11');
