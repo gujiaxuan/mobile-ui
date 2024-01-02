f63bcfbf8d826822710a392d37420aa768d2739c
gujiaxuan <1615258386@qq.com>
2024-01-02 11:21:47 +0800

    哈哈

--- src/pages/hoc/index.jsx
+++ src/pages/hoc/index.jsx
@@ -1,10 +1,14 @@
-import { Form, Input } from 'antd-mobile'
+import { useEffect, useState } from 'react';
 import styles from './demo2.less';
 
 export default () => {
-    return (
-        <div className={styles['demo']}>
-            啦啦啦
-        </div>
-    )
-}
+  const [hh, setHh] = useState({});
+
+  useEffect(() => {
+    if (hh) {
+      setHh('11');
+    }
+  }, []);
+
+  return <div className={styles['demo']}>啦啦啦</div>;
+};
